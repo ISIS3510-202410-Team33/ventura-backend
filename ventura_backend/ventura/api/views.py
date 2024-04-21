255b3995c9444a392873b8ae22dce6489d84801f
Obed Cabanzo <78177466+ObedCabanzo@users.noreply.github.com>
2024-04-21 13:24:55 -0500

    Implementation of patch in user_frequenciesViewSet

--- ventura_backend/ventura/api/views.py
+++ ventura_backend/ventura/api/views.py
@@ -1,3 +1,4 @@
+
 from django.http import JsonResponse
 from rest_framework import viewsets
 from ventura.models import College, College_location, User, User_calification, User_frequency
@@ -46,8 +47,11 @@ class user_calificationsViewSet(viewsets.ModelViewSet):
         location = self.request.query_params.get('college_location_id', None)
         if user is not None and location is not None:
             return User_calification.objects.filter(user_id=user , college_location_id=location)
-        if user is not None:
+        elif user is not None:
             return User_calification.objects.filter(user_id=user)
+        elif location is not None:
+            return User_calification.objects.filter(college_location_id=location)
+        
         return User_calification.objects.all()
 
 class user_frequenciesViewSet(viewsets.ModelViewSet):
@@ -59,18 +63,33 @@ class user_frequenciesViewSet(viewsets.ModelViewSet):
         location = self.request.query_params.get('college_location_id', None)
         if user is not None and location is not None:
             return User_frequency.objects.filter(user_id=user , college_location_id=location)
+        elif user is not None:
+            return User_frequency.objects.filter(user_id=user)
+        elif location is not None:
+            return User_frequency.objects.filter(college_location_id=location)
         return User_frequency.objects.all()
     
-    def patch(self, request):
-        print(self)
-        print(request)
+    def patch(self,request):
+
         user = self.request.query_params.get('user_id', None)
-        location = self.request.query_params.get('college_location_id', None)
+        location = self.request.query_params.get('location_id', None)
         
-        if user is not None and location is not None:
-            User_frequency.objects.filter(user_id=user, college_location_id=location).update(frequency=1)
-            return User_frequency.objects.filter(user_id=user, college_location_id=location)
-        return User_frequency.objects.none()
+        if user is None or location is None:
+            return JsonResponse(status = 400, data={"error": "There is no user_id or location_id in the request"})
+       # if no model exists by this PK, raise a 404 error
+        modelList = User_frequency.objects.filter(user_id=user, college_location_id=location)
+        if modelList.count() == 0:
+            return JsonResponse(status=404, data={"error": "No register exists by this user or location"})
+
+        model = modelList.first()
+        # this is the only field we want to update        
+        data = {"frequency": model.frequency + int(1)}
+        serializer = User_frequencySerializer(model, data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(status = 200, data=serializer.data)
+        # return a meaningful error response
+        return JsonResponse(status = 400, data={"error": "Bad request"})
     
 
     
