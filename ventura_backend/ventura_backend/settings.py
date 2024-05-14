a6bba6f1ef903680b856c992cbacf9151c810060
c4ts0up <a.baccap@uniandes.edu.co>
2024-05-14 14:52:33 -0500

    Use of environment variables

--- ventura_backend/ventura_backend/settings.py
+++ ventura_backend/ventura_backend/settings.py
@@ -83,11 +83,11 @@ WSGI_APPLICATION = "ventura_backend.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "",
-        "USER": "",
-        "PASSWORD": "",
-        "HOST": "",
-        "PORT": "",
+        "NAME": os.environ.get("DATABASE_NAME"),
+        "USER": os.environ.get("DATABASE_USER"),
+        "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
+        "HOST": os.environ.get("DATABASE_HOST"),
+        "PORT": os.environ.get("DATABASE_PORT"),
     }
 }
 
